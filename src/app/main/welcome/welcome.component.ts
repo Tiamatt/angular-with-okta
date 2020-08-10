0c417902987aaa2fea1d3ec868ee6fe09d416e0d
tiamatt <samira.yusifova@gmail.com>
2020-08-10 02:32:27 -0400

    Phase 1. Get info about the user

--- src/app/main/welcome/welcome.component.ts
+++ src/app/main/welcome/welcome.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
 
 @Component({
   selector: 'welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
+
 export class WelcomeComponent implements OnInit {
+  public userName: string;
 
-  constructor() { }
+  constructor(
+    public oktaAuthService: OktaAuthService,
+  ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    // returns an array of claims
+    const userClaims = await this.oktaAuthService.getUser();
+
+    console.log({'kaliLog': userClaims});
+
+    // user name is exposed directly as property
+    this.userName = userClaims.name;
   }
 
 }

3415d86d9fecf213a04746e2a22c6e01c5157127
tiamatt <samira.yusifova@gmail.com>
2020-08-11 18:07:53 -0400

    Attached header with okta bearer while calling api in user-details component

--- src/app/user-profile/user-details/user-details.component.ts
+++ src/app/user-profile/user-details/user-details.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { OktaAuthService } from '@okta/okta-angular';
+// custom services
+import { UserProfileService } from './../user-profile.service';
 
 @Component({
   selector: 'user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.scss']
 })
-export class UserDetailsComponent implements OnInit {
 
-  constructor() { }
+export class UserDetailsComponent implements OnInit, OnDestroy {
+  // public variables
+  public userProfileNames: string[] = null;
+  // private variables
+  private subscriptions = new Subscription();
 
-  ngOnInit() {
+  constructor(
+    private httpClient: HttpClient,
+      public oktaAuthService: OktaAuthService,
+  ) { }
+
+  private async populateUserProfileNames() {
+    
+   // let apiUrl = 'https://localhost:44349/api/messages';
+
+    let apiUrl = 'https://localhost:44375/api/userProfile/names';
+    const accessToken = await this.oktaAuthService.getAccessToken();
+    let header = {
+      headers: {
+        Authorization: 'Bearer ' + accessToken,
+      }
+    };
+
+    console.log({ "header_populateUserProfileNames": header });
+
+    this.httpClient.get(apiUrl, header).subscribe(
+      (data: any) => {
+        console.log({ "api_result_populateUserProfileNames": data });
+        this.userProfileNames = data;
+      }, (err) => {
+        console.log({ "api_error_populateUserProfileNames": err });
+      }
+    );
+
+  }
+
+  async ngOnInit() {
+    await this.populateUserProfileNames();
+  }
+
+  ngOnDestroy() {
+    this.subscriptions && this.subscriptions.unsubscribe();
   }
 
 }
